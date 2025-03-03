3e8a2a895ad35f67c7620be32ff3968beade02dc
Leandro Radusky <leandro.radusky@gmail.com>
2025-03-03 11:21:54 +0100

    Small changes to make Render work

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://cohorts_db_user:LNdjcSkFYnBPqeNAifTfUlihgtQY81FU@dpg-cv2nq2dsvqrc7395t2n0-a/cohorts_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
